2b8dca99e6f1efe5978fa334a764aaa2b4dd936f
praveenearle <praveen.earle@gmail.com>
2016-09-26 00:44:21 +0530

    angular js ng-dbl
    angular js ng-dbl

---
+++ angular js ng-double click/angular js s1a2/scripts/script.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.message = "Click the button";
+    $scope.myfunc = function () {
+        alert ("Button is double  clicked");
+    };
+
+         
+    }); 
+
